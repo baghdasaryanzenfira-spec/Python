bb4090413946200d2f36c3c5c172642d152a1299
baghdasaryanzenfira-spec <baghdasaryanzenfira@gmail.com>
2026-02-06 21:06:50 +0000

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+#while True:
+#    symbol=input("{+,-,*,/,c=exit} input symbol: ").lower()
+#    if symbol=="c":
+#        print("End!")
+#        break 
+#    n=int(input("Enter first number: "))
+#    m=int(input("Enter second number: "))
+#    if symbol=="*":
+#        print(f"{n}*{m}={n*m}")
+#    elif symbol=="/":
+#        print(f"{n}/{m}={n/m}")
+#    elif symbol=="+":
+#        print(f"{n}+{m}={n+m}")
+#    elif symbol=="-":
+#        print(f"{n}-{m}={n-m}")
+
+#2
+#while True:
+#    num=input("Enter a number or 'stop' to exit: ").lower()
+#    if num=='stop':
+#        print("Finish!")
+#        break
+#    if num==num[::-1]:
+#        print(f"{num} is a palindrome.")
+#    else:
+#        print(f"{num} is not a palindrome")
+
+#3
+#while True:
+#    word=input("Enter a number or 'stop' to exit: ").lower()
+#    if word=='stop':
+#        print("Finish!")
+#        break
+#    if len(word)>=5:
+#        print(word[::-1].upper())
+
+#4
