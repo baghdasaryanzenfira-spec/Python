6790b30a5063998e1a673419bc40f25dcff94bd8
baghdasaryanzenfira-spec <baghdasaryanzenfira@gmail.com>
2026-02-07 15:49:25 +0000

    Add a guessing game for the capital of France

--- cli.py
+++ cli.py
@@ -1,1 +1,20 @@
-
+#!/usr/bin/python3
+question = "What is the capital of France?"
+answer = "paris"
+hidden_answer = "-" * len(answer)
+print(question)
+while "-" in hidden_answer:
+    print(hidden_answer)
+    letter = input("Enter a letter: ").lower()
+    if letter in answer:
+        new_hidden_answer = ""
+        for i in range(len(answer)):
+            if answer[i] == letter:
+                new_hidden_answer += letter
+            else:
+                new_hidden_answer += hidden_answer[i]
+        hidden_answer = new_hidden_answer
+        print("Correct!")
+    else:
+        print("No such letter!")
+print(f"Win! {answer}")
